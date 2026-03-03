bc807807801e4f00d00e34f7d29a5423e62854bb
Om Singhal <23bcs184@iiitdmj.ac.in>
2026-03-03 17:52:52 +0530

    fix error

--- server/index.js
+++ server/index.js
@@ -8,8 +8,9 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
 }))
 app.use(express.json())
 app.use(cookiesParser())
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const { Server } = require('socket.io')
-const http  = require('http')
+const http = require('http')
 const getUserDetailsFromToken = require('../helpers/getUserDetailsFromToken')
 const UserModel = require('../models/UserModel')
-const { ConversationModel,MessageModel } = require('../models/ConversationModel')
+const { ConversationModel, MessageModel } = require('../models/ConversationModel')
 const getConversation = require('../helpers/getConversation')
 
 const app = express()
 
+// ✅ Add CORS middleware to Express
+const cors = require('cors')
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}))
+
 /***socket connection */
 const server = http.createServer(app)
-const io = new Server(server,{
-    cors : {
-        origin : process.env.FRONTEND_URL,
-        credentials : true
+const io = new Server(server, {
+    cors: {
+        origin: process.env.FRONTEND_URL || "http://localhost:5173",
+        credentials: true,
+        methods: ["GET", "POST"]
     }
 })
 
