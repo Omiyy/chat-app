3ecce952000f34e5ab6f07574cd8638569ebed8c
Om Singhal <23bcs184@iiitdmj.ac.in>
2026-03-03 19:18:17 +0530

    error fix

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin:  "https://chat-app-mu-ten-76.vercel.app" ||  process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
 }))
